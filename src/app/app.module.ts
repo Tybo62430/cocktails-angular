93d898327bd8374735a0c8aa0486714decc8d01a
thibault <derbomez.thibault@gmail.com>
2020-11-23 22:49:17 +0100

    creation du module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRouting } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { CocktailsListComponent } from './cocktail-container/cocktails-list/cocktails-list.component';
-import { CocktailDetailsComponent } from './cocktail-container/cocktail-details/cocktail-details.component';
-import { CocktailContainerComponent } from './cocktail-container/cocktail-container.component';
 import { ActiveDirective } from './shared/directives/active.directive';
 import { PanierComponent } from './panier/panier.component';
 import { IngredientsListComponent } from './panier/ingredients-list/ingredients-list.component';
 import { PanierService } from './shared/services/panier.service';
-import { CocktailEditComponent } from './cocktail-container/cocktail-edit/cocktail-edit.component';
-import { FilterPipe } from './shared/pipes/filter.pipe';
+
+import { CocktailModule } from './cocktail-container/cocktail.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CocktailsListComponent,
-    CocktailDetailsComponent,
-    CocktailContainerComponent,
     ActiveDirective,
     PanierComponent,
     IngredientsListComponent,
-    CocktailEditComponent,
-    FilterPipe,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRouting,
-    ReactiveFormsModule,
     HttpClientModule,
+    CocktailModule,
   ],
   providers: [PanierService],
   bootstrap: [AppComponent],
---
+++ src/app/cocktail-container/cocktail.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { CocktailsListComponent } from './cocktails-list/cocktails-list.component';
+import { CocktailDetailsComponent } from './cocktail-details/cocktail-details.component';
+import { CocktailContainerComponent } from './cocktail-container.component';
+import { CocktailEditComponent } from './cocktail-edit/cocktail-edit.component';
+import { FilterPipe } from '../shared/pipes/filter.pipe';
+
+@NgModule({
+  declarations: [
+    CocktailsListComponent,
+    CocktailDetailsComponent,
+    CocktailContainerComponent,
+    CocktailEditComponent,
+    FilterPipe,
+  ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
+  providers: [],
+  exports: [],
+})
+export class CocktailModule {}
